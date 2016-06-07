1b6b0e6684850de29ec56468c7fdfa25f3ef345c
siyu <astro6974@gmail.com>
2016-06-07 17:50:06 +0200

    fix merge

--- manager.php
+++ manager.php
@@ -3,22 +3,61 @@
 <head>
 	<title>
 		manager
-		<meta charset="utf-8">
 	</title>
+	<meta charset="utf-8">
 </head>
 <body>
-	<?php		
-	include "connect.php";
-	$vConn = fConnect();
-	$mail=$_GET["mail"];
-	$vSql="SELECT adresse, suface, nb_bureau_individuel,CASE WHEN actif='true' THEN 'Oui' ELSE 'Non' END FROM espace where id=(SELECT idmanager FROM manager where mail='$mail'";
-	$vQuery=pg_query($vConn, $vSql);	
-	echo '<table border='1' ';
-	echo '<tr><th></th><th>Adresse</th><th>Surface</th><th>nombre de bureaux individuels</th><th>actif ?</th><th>Date fin</th><th>Type</th></tr>';
-	while($result=pg_fetch_array($vQuery,NULL,PGSQL_BOTH)){
-		echo "<tr><td>$result[0]</td><td>$result[1]</td><td>$result[2]</td>";
-	}
-	echo '</table>';
+<fieldset>
+			<legend>Gestionnaire d'espaces</legend>
+	  <?php
+	  include "connect.php";
+	  $vConn = fConnect();
+	  $mail=$_GET["mail"];
+	  $vSql="SELECT adresse, surface, nb_bureau_individuel,CASE WHEN actif='true' THEN 'Oui' ELSE 'Non' END FROM espace where id=(SELECT idmanager FROM manager where mail='$mail')";
+	  $vQuery=pg_query($vConn, $vSql);
+	  echo "<table border='1' ";
+	  echo '<tr><th>Adresse</th><th>Surface</th><th>nombre de bureaux individuels</th><th>actif ?</th></tr>';
+	  while($result=pg_fetch_array($vQuery,NULL,PGSQL_BOTH)){
+		  echo "<tr><td>$result[0]</td><td>$result[1]</td><td>$result[2]</td><td>$result[3]</td></tr>";
+	  }
+	   	echo '</table>';
+	  ?>
+</fieldset>
+<fieldset>
+	  <legend>Gestionnaire de formules </legend>
+	<?php
+	$vSql="SELECT e.adresse,f.nom FROM espace e JOIN assoc_propose a ON e.idespace=a.id_espace JOIN formule f ON f.nom=a.nom_formule WHERE
+	 e.id=(SELECT idmanager FROM manager where mail='$mail') ORDER BY e.adresse";
+	 $vQuery=pg_query($vConn,$vSql);
+	 echo "<table border='1' ";
+	echo '<tr><th>Adresse</th><th>Formule</th></tr>';
+	 while($result=pg_fetch_array($vQuery,null,PGSQL_BOTH)){
+	  echo "<tr><td>$result[0]</td><td>$result[1]</td></tr>";
+	 }
+	 	echo '</table>';
 	?>
+	</fieldset>
+	<fieldset>
+	  <legend> Attribution de salle pour les conférences</legend>
+	  <?php
+	$vSql="SELECT titre,datec,resume FROM conference WHERE id_espace IS NULL";
+	 $vQuery=pg_query($vConn,$vSql);
+	 echo "<table border='1' ";
+	echo '<tr><th>Titre</th><th>Date</th><th>Résumé</th></tr>';
+	 while($result=pg_fetch_array($vQuery,null,PGSQL_BOTH)){
+	  echo "<tr><td>$result[0]</td><td>$result[1]</td><td>$result[2]</td></tr>";
+	 }
+
+	?>
+	</table>
+		<form method="post" action="modifierConference.php">
+		Titre de la conférence <input type="text" name="titre" id="titre"/>
+		Date de la conférence <input type="text" name="date" id="date"/>
+    Salle à attribuer <input type="text" name="salle" id="salle"/>
+   <?php echo "<input type='hidden' name='mail' value='$mail'/>"; ?>
+   <input type="submit" name="Attribuer la salle"/>
+	</form>
+
+	</fieldset>
 </body>
 </html>
